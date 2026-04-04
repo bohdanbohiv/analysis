e210abaad764b6c79b2691091da9212e84a681d1
Bohdan Bohiv <bohiv.bohdan@gmail.com>
2026-04-04 13:14:57 +0200

    Read Appendix A.4

--- Analysis/Appendix_A_4.lean
+++ Analysis/Appendix_A_4.lean
@@ -9,65 +9,69 @@ Some examples of how variables and quantifiers are used in Lean
 
 namespace VariableExample1
 
-variable (X:Type) (x y:X)
+variable (X : Type) (x y : X)
 
 #check x = x
 #check x = y
 
--- #check x+y = y+x   -- this will not typecheck
+-- #check x+y = y+x  -- this will not typecheck
 
 end VariableExample1
 
 namespace VariableExample2
 
-variable (x:ℝ)
+variable (x : ℝ)
 
-#check x+3
-#check x+3 = 5
+#check x + 3
+#check x + 3 = 5
 
-example (h: x = 2) : x + 3 = 5 := by linarith
+example (h : x = 2) : x + 3 = 5 := by
+  rewrite[h]
+  norm_num1
 
-example (h: x ≠ 2) : x + 3 ≠ 5 := by
+example (h : x ≠ 2) : x + 3 ≠ 5 := by
   contrapose! h
   linarith
 
-example : ¬ ∀ (x:ℝ), x + 3 = 5 := by
-  simp
+example : ¬∀ (x : ℝ), x + 3 = 5 := by
+  rewrite [not_forall]
   use 0
-  simp
+  norm_num1
 
-example : ¬ ∀ (x:ℝ), x + 3 ≠ 5 := by
-  simp
+example : ¬∀ (x : ℝ), x + 3 ≠ 5 := by
+  rewrite [not_forall]
   use 2
-  norm_num
+  norm_num1
 
-example : ∃ (x:ℝ), x + 3 = 5 := by
+example : ∃ (x : ℝ), x + 3 = 5 := by
   use 2
-  norm_num
+  norm_num1
 
-example : ∀ (x:ℝ), (x+1)^2 = x^2 + 2*x + 1 := by
+example : ∀ (x : ℝ), (x + 1)^2 = x^2 + 2*x + 1 := by
   intro x
   ring
 
 end VariableExample2
 
-/-- A dummy statement is in place here for this example.-/
+/-- A dummy statement is in place here for this example. -/
 example : 0 = 0 := by
   set x := 342
   have : x + 155 = 497 := by
     unfold x
-    norm_num
+    rfl
   rfl
 
-example : ¬ ∀ (x:ℝ), x + 155 = 497 := by
-  simp
+example : ¬∀ (x : ℝ), x + 155 = 497 := by
+  rewrite [not_forall]
   use 0
-  norm_num
+  norm_num1
 
-example : ¬ ∀ x > (0:ℝ), x^2 > x := by
-  simp
+example : ¬∀ x > (0 : ℝ), x^2 > x := by
+  rewrite [not_forall]
   use 0.5
-  norm_num
+  rewrite [Classical.not_imp]
+  norm_num1
+  exact ⟨True.intro, True.intro⟩
 
 /- The code below will not typecheck.
 
@@ -77,19 +81,21 @@ example : ∀ (x:ℝ), x + 3 = 5 := by
 
 -/
 
-example : ∀ x, (3 < x ∧ x < 2) → (6 < 2*x ∧ 2*x < 4) := by
+example : ∀ x, 3<x ∧ x<2 → 6<2*x ∧ 2*x<4 := by
   intro x h
-  obtain ⟨ h1, h2 ⟩ := h
-  -- the previous three lines can be golfed to `intro x ⟨ h1, h2 ⟩`
+  obtain ⟨h1, h2⟩ := h
+  -- the previous three lines can be golfed to `intro x ⟨h1, h2⟩`
   constructor
-  . linarith
+  · linarith
   linarith
 
-example : ∃ (x:ℝ), x^2 + 2*x - 8 = 0 := by
+example : ∃ (x : ℝ), x^2 + 2*x - 8 = 0 := by
   use 2
-  norm_num
+  norm_num1
 
-example : ¬ ∃ x, (3 < x ∧ x < 2) ∧ (6 < 2*x ∧ 2*x < 4) := by
-  simp
-  intro x h1 h2 h3
+example : ¬∃ x, (3 < x ∧ x < 2) ∧ (6 < 2*x ∧ 2*x < 4) := by
+  rewrite [not_exists]
+  intro x
+  rewrite [not_and, not_and]
+  intro ⟨h1, h2⟩
   linarith
