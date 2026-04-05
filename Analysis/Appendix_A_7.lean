9f77c21eb11599bc9ee9b535eec87bcb5918c4ba
Bohdan Bohiv <bohiv.bohdan@gmail.com>
2026-04-05 19:39:08 +0200

    Read Appendix A.7

--- Analysis/Appendix_A_7.lean
+++ Analysis/Appendix_A_7.lean
@@ -18,35 +18,33 @@ example : ∑' n:ℕ, 9*(10:ℝ)^(-(n:ℤ)-1) = 1 := by
   rw [tsum_geometric_of_lt_one (by norm_num) (by norm_num)]
   norm_num
 
-example : 12 = (2:Fin 10)  := by
+example : 12 = (2 : Fin 10)  := by
   decide
 
-
 /-- Reflexive axiom -/
-example {X:Type} (x:X) : x = x := by
-  rfl
+example {X : Type} (x : X) : x = x := rfl
 
 #check Eq.refl
 
 /-- Symmetry axiom -/
-example {X:Type} (x y:X) (h: x = y) : y = x := by
+example {X : Type} (x y : X) (h : x = y) : y = x := by
   rw [h]
 
 #check Eq.symm
 
 /-- Transitivity axiom -/
-example {X:Type} (x y z:X) (h1: x = y) (h2: y = z) : x = z := by
+example {X : Type} (x y z : X) (h1 : x = y) (h2 : y = z) : x = z := by
   rw [h1, h2]
 
 #check Eq.trans
 
 /-- Substitution axiom -/
-example {X Y:Type} (f:X → Y) (x y:X) (h: x = y) : f x = f y := by
+example {X Y : Type} (f : X → Y) (x y : X) (h : x = y) : f x = f y := by
   rw [h]
 
 #check congrArg
 
-def equality_as_equiv_relation (X:Type) : Setoid X := {
+def equality_as_equiv_relation (X : Type) : Setoid X := {
   r a b := a = b
   iseqv := {
     refl := Eq.refl
@@ -55,28 +53,23 @@ def equality_as_equiv_relation (X:Type) : Setoid X := {
   }
 }
 
-
 open Real in
 /-- Example A.7.1 -/
-example {x y:ℝ} (h:x = y) : 2*x = 2*y ∧ sin x = sin y ∧ ∀ z, x + z = y + z := by
+example {x y : ℝ} (h : x = y) : 2*x = 2*y ∧ sin x = sin y ∧ ∀ z, x + z = y + z := by
   split_ands
-  . rw [h]
-  . rw [h]
+  · rw [h]
+  · rw [h]
   intro z
   rw [h]
 
 /-- Example A.7.2 -/
-example {n m:ℤ} (hn: Odd n) (h: n = m) : Odd m := by
-  rw [h] at hn
-  exact hn
-
-example {n m k:ℤ} (hnk: n > k) (h: n = m) : m > k := by
-  rw [h] at hnk
-  exact hnk
+example {n m k : ℤ} (hn : Odd n) (hnk : n > k) (h : n = m) : Odd m ∧ m > k := by
+  rewrite [←h]
+  exact ⟨hn, hnk⟩
 
 open Real in
-example {x y z:ℝ} (hxy : x = sin y) (hyz : y = z^2) : x = sin (z^2) := by
-  have : sin y = sin (z^2) := by rw [hyz]
+example {x y z : ℝ} (hxy : x = sin y) (hyz : y = z^2) : x = sin (z^2) := by
+  have : sin y = sin (z^2) := congr(sin $hyz)
   exact hxy.trans this
 
 abbrev make_twelve_equal_two : ℤ → ℤ → Prop := fun a b ↦ a = 12 ∧ b = 2
@@ -109,5 +102,5 @@ abbrev NewInt.quot {X:Type} {f: ℤ → X} (hf: f 12 = f 2) : NewInt → X := by
 example {X:Type} {f:ℤ → X} (hf: f 12 = f 2) (n:ℤ) : NewInt.quot hf (n:NewInt) = f n := rfl
 
 /-- Exercise A.7.1 -/
-example {a b c d:ℝ} (hab: a = b) (hcd : c = d) : a + d = b + c := by
+example {a b c d : ℝ} (hab : a = b) (hcd : c = d) : a + d = b + c := by
   sorry
